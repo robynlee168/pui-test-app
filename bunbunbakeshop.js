71e9b88544c0685347a002c9081ac25b7e778a90
robynlee168 <91481505+robynlee168@users.noreply.github.com>
2021-11-06 23:10:34 -0400

    update cart page

--- bunbunbakeshop.js
+++ bunbunbakeshop.js
@@ -9,12 +9,69 @@ function calculateCost() {
     productCost[0].innerHTML = "$" + cost.toFixed(2);
 }
 
-
-//visual indication of number of items in cart 
-var count = 0;
-
 function submitCount() {
-    count = count + 1;
-    var cartNum= document.getElementsByClassName("cart-num");
-    cartNum[0].innerHTML = count;
+    var order = {product:"", quantity:0, glaze:"None", cost:0};
+    order.product = document.getElementsByClassName("product-title")[0].innerHTML;
+    var quantity = document.getElementById("quantity-select");
+    order.quantity = quantity.options[quantity.selectedIndex].value;
+    var glaze = document.getElementById("glaze-select");
+    order.glaze = glaze.options[glaze.selectedIndex].value;
+    order.cost = order.quantity * 3.5;
+
+    //visual indication of number of items in cart 
+    var ordersInCart = JSON.parse(localStorage.getItem("orders"));
+    if (ordersInCart == null)
+        ordersInCart = [];
+    ordersInCart.push(order);
+    console.log(ordersInCart);
+    localStorage.setItem("orders", JSON.stringify(ordersInCart));
+    var cartNum = document.getElementsByClassName("cart-num");
+    cartNum[0].innerHTML = ordersInCart.length; 
 }
+
+function  updateCart() {
+    var ordersInCart = JSON.parse(localStorage.getItem("orders"));
+    if (ordersInCart) {
+        var cartNum = document.getElementsByClassName("cart-num");
+        cartNum[0].innerHTML = ordersInCart.length
+    }
+}
+
+function getFromLocalStorage() {
+    var ordersInCart = JSON.parse(localStorage.getItem('orders'));
+    var divString = "";
+    var total = 0;
+    if (ordersInCart) {
+        for (let i=0; i < ordersInCart.length; i++) {
+            divString += '<img src="menu-item.png" alt="original cinnamon roll">';
+            divString += '<span class= "cart-product">' + ordersInCart[i].product + '</span>';
+            divString += '<span class= "cart-cost">' + ordersInCart[i].cost + '</span>';
+            divString += '<p class="cart-glaze"> w/' + ordersInCart[i].glaze + '</p>';
+            divString += '<i onclick="removeFromCart(' + i + ');" class="fas fa-trash-alt"></i>';
+            divString += '<p class="cart-quantity">Quantity: ' + ordersInCart[i].quantity + '</p>';
+            divString += '<hr>';
+            total += ordersInCart[i].cost;
+        }
+        var cartSummary = document.getElementsByClassName("cart-summary");
+        cartSummary[0].innerHTML = divString;
+        var cartTotal = document.getElementById("cart-total");
+        cartTotal.innerHTML = total; 
+    }
+}
+
+function removeFromCart(i) {
+    console.log("call removeFromCart() i=" + i);
+    var ordersInCart = JSON.parse(localStorage.getItem("orders"));
+    ordersInCart.splice(i, 1);
+    localStorage.setItem('orders', JSON.stringify(ordersInCart));
+    location.reload();
+}
+
+// function addToLocalStorage() {
+//     for (let i = 0; i < quantitiesAddedToCart.length; i++) {
+//         localStorage.setItem("quantity"+i, quantitiesAddedToCart[i]);
+//         localStorage.setItem("glaze"+i, glazesAddedToCart[i]);
+//         localStorage.setItem("cost"+i, costsAddedToCart[i]);
+//     }
+// }
+
